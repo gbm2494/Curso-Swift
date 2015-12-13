5e6b4395c65c89c6809d3702a3414bfffa17c268
Gaudy Blanco <gbm2494@vancouver.local>
2015-12-13 00:56:57 -0600

    código final con comentarios

--- miniRetoMod2.playground/Contents.swift
+++ miniRetoMod2.playground/Contents.swift
@@ -4,19 +4,24 @@ Mini reto modulo 2
 
 import UIKit
 
+//ciclo para generar los 100 números
 for var i = 0; i <= 100; i++
 {
+    //if que controla que el nùmero sea divisible entre 5
     if i % 5 == 0 {
         print("\(i) Bingo!!!")
     }
+    //if que controla que el número sea par
     else if i % 2 == 0
     {
         print("\(i) par!!!")
     }
+    //if que controla que el número sea impar
     else if i % 2 != 0
     {
         print("\(i) impar!!!")
     }
+    //if que controla que el número esté entre 30 y 40
     else if 30 <= i && i <= 40
     {
         print("\(i) Viva Swift!!!")
