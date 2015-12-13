ba42e7ff9a9e2632d5b5ea7f908de49720447157
Gaudy Blanco <gbm2494@vancouver.local>
2015-12-13 00:54:50 -0600

    tarea completa

--- miniRetoMod2.playground/Contents.swift
+++ miniRetoMod2.playground/Contents.swift
@@ -4,4 +4,21 @@ Mini reto modulo 2
 
 import UIKit
 
-var str = "Hello, playground"
+for var i = 0; i <= 100; i++
+{
+    if i % 5 == 0 {
+        print("\(i) Bingo!!!")
+    }
+    else if i % 2 == 0
+    {
+        print("\(i) par!!!")
+    }
+    else if i % 2 != 0
+    {
+        print("\(i) impar!!!")
+    }
+    else if 30 <= i && i <= 40
+    {
+        print("\(i) Viva Swift!!!")
+    }
+ }
