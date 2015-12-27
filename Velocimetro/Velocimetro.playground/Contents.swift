0313df61984bec1fd1b99aefc22782d6e9cfaa5c
Gaudy Blanco <gbm2494@vancouver.local>
2015-12-27 00:27:03 -0600

    cambios tarea

--- Velocimetro/Velocimetro.playground/Contents.swift
+++ Velocimetro/Velocimetro.playground/Contents.swift
@@ -25,4 +25,41 @@ class Auto{
         /*Aquí se inicializa la enumeración con el valor inicial de apagado*/
        velocidad = Velocidades(velocidadInicial: Velocidades.Apagado)
     }
+    
+    /*Función para el cambio de velocidad que retorna la velocidad en km y el tipo de velocidad al que cambió*/
+    func cambioDeVelocidad( ) -> ( actual : Int, velocidadEnCadena: String){
+        
+        var velocidadString : String = ""
+        var anterior : Int = 0
+        
+        if velocidad.rawValue == 0 {
+            
+            velocidad = Velocidades(velocidadInicial: Velocidades.VelocidadBaja)
+            velocidadString = "Velocidad baja"
+        }
+        else if velocidad.rawValue == 20{
+            velocidad = Velocidades(velocidadInicial: Velocidades.VelocidadMedia)
+            velocidadString = "Velocidad Media"
+        }
+        else if velocidad.rawValue == 50{
+            velocidad = Velocidades(velocidadInicial: Velocidades.VelocidadAlta)
+            velocidadString = "Velocidad Alta"
+        }
+        else if velocidad.rawValue == 120 {
+            velocidad = Velocidades(velocidadInicial: Velocidades.VelocidadMedia)
+            velocidadString = "Velocidad Media"
+        }
+        
+        return (velocidad.rawValue, velocidadString)
+    }
 }
+
+var carro = Auto()
+
+for var i = 0 ; i < 20; i++
+{
+    var tupla = carro.cambioDeVelocidad()
+    print(tupla.actual,  tupla.velocidadEnCadena)
+}
+
+
