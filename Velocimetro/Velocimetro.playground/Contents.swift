b7be18621d18b3d8bf1e0730bf34c1851c10d06b
Gaudy Blanco <gbm2494@vancouver.local>
2015-12-27 00:09:48 -0600

    Inicio de la tarea

--- Velocimetro/Velocimetro.playground/Contents.swift
+++ Velocimetro/Velocimetro.playground/Contents.swift
@@ -1,5 +1,28 @@
-//: Playground - noun: a place where people can play
+//: Minireto velocímetro Gaudy Blanco
 
 import UIKit
 
-var str = "Hello, playground"
+/*Enumeración de las velocidades de un auto*/
+enum Velocidades : Int{
+    
+    /*Distintos case para las diferentes velocidades del auto*/
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    /*inicializador que recibe la velocidad inicial que tendrá la enumeración*/
+    init(velocidadInicial : Velocidades){
+        self = velocidadInicial
+    }
+}
+
+/*Clase Auto*/
+class Auto{
+    
+    /*Se crea la instancia de la enumeración*/
+    var velocidad : Velocidades
+    
+    /*inicializador de la clase Auto*/
+    init(){
+        /*Aquí se inicializa la enumeración con el valor inicial de apagado*/
+       velocidad = Velocidades(velocidadInicial: Velocidades.Apagado)
+    }
+}
